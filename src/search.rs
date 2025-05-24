03d1e457d2f162c6386e3bb9bb2463844e41f04c
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-05-24 17:36:22 -0400

    Bonus for moves that evade pawn threat Bench: 17858198

--- src/search.rs
+++ src/search.rs
@@ -236,6 +236,20 @@ impl MCTS {
         } else {
             0.0
         };
+
+        let pawn_threat_evasion = if pawn_protected.has(mv.from_sq()) && !pawn_protected.has(mv.to_sq()) {
+            match moving_piece.piece_type() {
+                PieceType::Pawn => 0.4,
+                PieceType::Knight => 1.2,
+                PieceType::Bishop => 1.2,
+                PieceType::Rook => 2.4,
+                PieceType::Queen => 3.5,
+                _ => 0.0,
+            }
+        } else {
+            0.0
+        };
+        
         let moving_piece = self.position.board().piece_at(mv.from_sq()).unwrap();
         let psqt = if mv.kind() != MoveKind::Promotion {
             psqt_score(self.position.board(), moving_piece.piece_type(), mv.to_sq())
@@ -258,7 +272,7 @@ impl MCTS {
             0.0
         };
 
-        cap_bonus + promo_bonus - pawn_protected_penalty + psqt as f32 / 100.0
+        cap_bonus + promo_bonus + pawn_threat_evasion - pawn_protected_penalty + psqt as f32 / 100.0
     }
 
     fn expand_node(&mut self, node_idx: u32) {
