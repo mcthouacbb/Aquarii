537e0c014d18f9142d2d85d057df43c5e5a6b44f
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-06-16 21:29:20 -0400

    0.55 exploration tau Bench: 23815210

--- src/search.rs
+++ src/search.rs
@@ -190,7 +190,7 @@ impl MCTS {
                         1.0 - child.q()
                     };
                     let policy = child.policy;
-                    let expl = (node.visits as f32).sqrt() / (1 + child.visits) as f32;
+                    let expl = ((node.visits as f32).ln() * 0.55).exp() / (1 + child.visits) as f32;
                     let cpuct = if root { Self::ROOT_CPUCT } else { Self::CPUCT };
                     let uct = q + cpuct * policy * expl;
 
