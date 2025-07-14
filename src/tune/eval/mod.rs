db9b25d53b85e7fff6df1242c80b597a0c96f250
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-07-13 22:57:25 -0400

    Add eval tracing

--- src/tune/eval/mod.rs
+++ src/tune/eval/mod.rs
@@ -1,13 +1,16 @@
+use std::fs::File;
+
 mod data;
 mod trace;
+mod tune;
 
 pub fn main(filenames: &[String]) {
-    /*let mut files = Vec::with_capacity(filenames.len());
+    let mut files = Vec::with_capacity(filenames.len());
     for filename in filenames {
         files.push(File::open(filename).expect("Unable to open policy data file"));
     }
 
-    let dataset = data::load_dataset(files.as_slice());*/
+    let dataset = data::load_dataset(files.as_slice());
     let params = &trace::zero_params();
 	println!("{}", trace::EvalFeature::format_all_features(params));
     /*println!(
