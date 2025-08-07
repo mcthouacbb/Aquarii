5d82f9f7442b77c8c6177a95fb23f44d02c02329
mcthouacbb <mcthouacbb@gmail.com>
2025-08-07 10:52:41 -0400

    Eval params tuned on 26 superbatches + increase bench depth Bench: 11548011

--- src/bench.rs
+++ src/bench.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use std::time::Instant;
 
-const BENCH_DEPTH: i32 = 6;
+const BENCH_DEPTH: i32 = 7;
 const BENCH_POSITIONS: [&'static str; 50] = [
     "r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq a6 0 14",
     "4rrk1/2p1b1p1/p1p3q1/4p3/2P2n1p/1P1NR2P/PB3PP1/3R1QK1 b - - 2 24",
